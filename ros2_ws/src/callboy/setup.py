b073af8b7709b73fb05d050a435d1cf6f011e196
Bigfire3 <zankerfabian5@gmail.com>
2026-01-22 02:19:22 +0800

    Implement Riva Node

--- ros2_ws/src/callboy/setup.py
+++ ros2_ws/src/callboy/setup.py
@@ -26,6 +26,7 @@ setup(
             "callboy_ollama_planner = callboy.nodes.ollama_planner:main",
             "callboy_sdk2_executor = callboy.nodes.sdk2_executor_node:main",
             "callboy_udp_audio_topic = callboy.nodes.udp_audio_topic_publisher:main",
+            "callboy_riva_asr = callboy.nodes.riva_asr_node:main",
         ],
     },
 )
